74ba0eef65f9e6d23da5995a5fe4fb1f968d7792
aodioicq <aodioicq@gmail.com>
2013-02-25 21:23:16 -0500

    correct host

--- src/edu/nyu/cs/cs2580/QueryHandler.java
+++ src/edu/nyu/cs/cs2580/QueryHandler.java
@@ -37,11 +37,11 @@ class QueryHandler implements HttpHandler {
 	  if(format.equals("text")){
 		  for(ScoredDocument sd:documents){
 	        	result+=query+"\t"+sd._did+"\t"+sd._title+"\t"+sd._score+"\n";
-	        }
+	      }
 	  }else{
 		  result+="<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\" \"http://www.w3.org/TR/html4/loose.dtd\"><html><body>";
 		  for(ScoredDocument sd:documents){
-			  result+="Document id: "+sd._did+"\tTitle: "+sd._title+"\n<hr/>\n";
+			  result+="Document id: "+sd._did+"       Title: "+sd._title+"\n<hr/>\n";
 		  }
 		  result+="</body></html>";
 		  
