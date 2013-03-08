58c16c3774a1f4c5a3f5d6c3ffa9cd9ebf3cdefb
aodioicq <aodioicq@gmail.com>
2013-03-08 15:32:25 -0500

    documentIndexed

--- src/edu/nyu/cs/cs2580/DocumentIndexed.java
+++ src/edu/nyu/cs/cs2580/DocumentIndexed.java
@@ -1,13 +1,19 @@
 package edu.nyu.cs.cs2580;
 
+import java.util.Vector;
+
 /**
  * @CS2580: implement this class for HW2 to incorporate any additional
  * information needed for your favorite ranker.
  */
 public class DocumentIndexed extends Document {
   private static final long serialVersionUID = 9184892508124423115L;
+  public int bodySize;//Document body size
+  public Vector<Integer> documentTermFrequency;//the term frequency of all terms in query
 
   public DocumentIndexed(int docid) {
     super(docid);
+    documentTermFrequency=new Vector<Integer>();
   }
+
 }
