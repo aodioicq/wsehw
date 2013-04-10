a472d2b936d1ecc7fed26674e24f26b516a255fd
anguo <al3096@nyu.edu>
2013-04-10 17:51:18 -0300

    Update LogMinerNumviews.java

--- src/edu/nyu/cs/cs2580/LogMinerNumviews.java
+++ src/edu/nyu/cs/cs2580/LogMinerNumviews.java
@@ -1,6 +1,14 @@
 package edu.nyu.cs.cs2580;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map.Entry;
+import java.util.TreeMap;
 
 import edu.nyu.cs.cs2580.SearchEngine.Options;
 
@@ -8,6 +16,8 @@ import edu.nyu.cs.cs2580.SearchEngine.Options;
  * @CS2580: Implement this class for HW3.
  */
 public class LogMinerNumviews extends LogMiner {
+  private HashMap<Integer, Integer> numViewMap = new HashMap<Integer, Integer>();
+  private HashMap<String, Integer> postingMap = new HashMap<String, Integer>();
 
   public LogMinerNumviews(Options options) {
     super(options);
@@ -29,9 +39,62 @@ public class LogMinerNumviews extends LogMiner {
   @Override
   public void compute() throws IOException {
     System.out.println("Computing using " + this.getClass().getName());
+    	readPosting();
+      	String line;
+		String[] parseLine;
+		File log = new File("data/log/20130301-160000.log");
+		BufferedReader br = new BufferedReader(new FileReader(
+				log.getAbsoluteFile()));
+
+		while ((line = br.readLine()) != null) {
+			parseLine = line.split(" ");
+
+			if (postingMap.containsKey(parseLine[1])) {
+				numViewMap.put(postingMap.get(parseLine[1]),
+						Integer.parseInt(parseLine[2]));
+			}
+		}
+		br.close();
+		write();
     return;
   }
+  private void readPosting() throws IOException {
+	 		String line;
+			String[] parseLine;
+			File post = new File("data/postingmap.txt");
+			BufferedReader br = new BufferedReader(new FileReader(
+					post.getAbsoluteFile()));
 
+			while ((line = br.readLine()) != null) {
+				parseLine = line.split("\t");
+				postingMap.put(parseLine[0],Integer.parseInt(parseLine[1]));		
+			}
+			br.close();
+  }
+  private void write() throws IOException{
+	
+	    
+      File numview = new File("data/numview.txt");
+        if (!numview.exists()) {
+			numview.createNewFile();
+		}
+        String newline = System.getProperty("line.separator");
+		String out = " ";
+		TreeMap<Integer, Integer> tm = new TreeMap<Integer, Integer>(numViewMap);
+
+		BufferedWriter bw = new BufferedWriter(new FileWriter(
+				numview.getAbsoluteFile()));
+
+		for (Entry<Integer, Integer> entry : tm.entrySet()) {
+			out = entry.getKey().toString() + "\t"
+					+ entry.getValue().toString();
+			bw.write(out);
+			bw.write(newline);
+		}
+		bw.close();
+	}
+   
+  
   /**
    * During indexing mode, this function loads the NumViews values computed
    * during mining mode to be used by the indexer.
@@ -40,7 +103,21 @@ public class LogMinerNumviews extends LogMiner {
    */
   @Override
   public Object load() throws IOException {
-    System.out.println("Loading using " + this.getClass().getName());
-    return null;
+	  System.out.println("Loading using " + this.getClass().getName());
+		String numViews = "data/numview.txt";
+			String line;
+			String[] parsed;
+			File nv = new File(numViews);
+			BufferedReader br = new BufferedReader(new FileReader(
+					nv.getAbsoluteFile()));
+			while ((line = br.readLine()) != null) {
+				parsed = line.split("\t");
+				numViewMap.put(Integer.parseInt(parsed[0]), Integer.parseInt(parsed[1]));
+			}
+			br.close();
+	  return null;
   }
+  
+
+		
 }
